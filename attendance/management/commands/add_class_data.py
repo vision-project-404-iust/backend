9bc652cf50533bdf4fa3e3543688933f53cc4322
MohammadHossein Haghdadi <mohammad.haghdadi@divar.ir>
2025-08-16 01:50:49 +0330

    add add_class_data logic

--- attendance/management/commands/add_class_data.py
+++ attendance/management/commands/add_class_data.py
@@ -1,11 +1,14 @@
+from math import inf
 from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
 import os
-import cv2
-import json
 from attendance.models import StudentData
-from datetime import datetime
+from typing import List, Tuple
+from ultralytics import YOLO
+from deepface import DeepFace
+import cv2
 
+TRACK_MODEL_PATH = "models/yolov8n-face-lindevs.onnx"
+DB_PATH = "models/db"
 
 class Command(BaseCommand):
     """
@@ -54,4 +57,121 @@ class Command(BaseCommand):
         self.stdout.write(f'Class ID: {class_id}')
         self.stdout.write(f'Frame interval: {frame_interval}')
         
+        self.track_model = YOLO(TRACK_MODEL_PATH)
+        
+        frames = self.get_frames(video_path, frame_interval)
+        
+        track_id_matches = {}
+        
+        data_set = []
+        
+        for frame_id, frame in enumerate(frames):
+            for track_id, box in self.get_boxes(frame):
+                if track_id not in track_id_matches:
+                    track_id_matches[track_id] = {}
+                
+                emotions : List[Tuple[str, float]] = self.get_emotion(box, frame)
+                
+                data_set.append((track_id, frame_id, emotions))
+                
+                top_match : List[Tuple[str, float]] = self.get_top_match(box, frame)
+
+                for match, distance in top_match:
+                    track_id_matches[track_id].setdefault(match, []).append(distance)
+
+        track_student_mapping = self.get_track_id_student_mapping(track_id_matches)
+
+        for track_id, frame_id, emotions in data_set:
+            student = track_student_mapping[track_id]
+            StudentData.objects.create(
+                student=student,
+                class_id=class_id,
+                frame_id=frame_id,
+                emotions=emotions
+            )
+
+
+    def get_frames(self, video_path, frame_interval):
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frames.append(frame)
+        cap.release()
+        return frames
     
+    def get_boxes(self, frame):
+        results = self.track_model.track(frame, persist=True, classes=[0])
+        result = results[0]
+
+        boxes = result.boxes.xywh.cpu().numpy().astype(int).tolist()
+        track_ids = result.boxes.id.cpu().numpy().astype(int).tolist()
+
+        return zip(track_ids, boxes)
+
+    
+    def get_emotion(self, box, frame):
+        x, y, w, h = box
+
+        x1 = max(x - w // 2, 0)
+        y1 = max(y - h // 2, 0)
+        x2 = min(x + w // 2, frame.shape[1])
+        y2 = min(y + h // 2, frame.shape[0])
+        face_crop = frame[y1:y2, x1:x2]
+        
+        analysis = DeepFace.analyze(face_crop, actions=['emotion'], enforce_detection=False)
+
+        return analysis['emotion']
+
+
+    def get_top_match(self, box, frame):
+        x, y, w, h = box
+
+        x1 = max(x - w // 2, 0)
+        y1 = max(y - h // 2, 0)
+        x2 = min(x + w // 2, frame.shape[1])
+        y2 = min(y + h // 2, frame.shape[0])
+        face_crop = frame[y1:y2, x1:x2]
+        
+        df_find = DeepFace.find(
+                img_path=face_crop,
+                db_path=DB_PATH,
+                detector_backend="mtcnn",
+                model_name="ArcFace",
+                enforce_detection=False,
+                silent=True
+        )
+            
+        df = df_find[0]
+        df = df.head(3)
+        
+        top_match = []
+        
+        for _, match in df.iterrows():
+            matched_img_path = match['identity']
+            distance = match['distance']
+
+            top_match.append((matched_img_path, distance))
+
+        return top_match
+        
+    def get_track_id_student_mapping(self, track_id_matches):
+        track_student_mapping = {}
+        
+        for track_id, students_distacne in track_id_matches.items():
+            best_student = ""
+            best_count = 0
+            best_avg = inf
+            for student, distance_list in students_distacne.items():
+                count = len(distance_list)
+                avg = sum(distance_list) / count
+                
+                if best_student == "" or count > best_count or (count == best_student and avg < best_avg):
+                    best_student = student
+                    best_count = count
+                    best_avg = avg
+            
+            track_student_mapping[track_id] = student
+        return track_student_mapping
